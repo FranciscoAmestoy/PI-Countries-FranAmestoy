e8f5ce17e297007f69b999628c0c15766f8c9cca
Francisco Amestoy <flyc0de92@gmail.com>
2022-06-03 23:06:06 -0300

    Back funcionando

---
+++ api/src/getApiInfo/getApiInfo.js
@@ -0,0 +1,37 @@
+// const { Router } = require('express');
+const axios = require("axios");
+const { Country } = require("../db");
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+// const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+const getApiInfo = async () => {
+  try {
+    const apiUrl = await axios.get("https://restcountries.com/v3/all");
+    const apiInfo = await apiUrl.data.map((country) => {
+      return {
+        id: country.cca3,
+        name: country.name.official,
+        image: country.flags[0],
+        continent: country.region,
+        capital: country.capital ? country.capital[0] : "no capital loaded",
+        subregion: country.subregion
+          ? country.subregion
+          : "no subregion loaded",
+        population: country.population,
+      };
+    });
+    await Country.bulkCreate(apiInfo);
+    console.log("Api loaded to DB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  getApiInfo,
+};
---
+++ api/src/routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+const getCountries = require("./getCountries.js");
+const getCountryId = require("./getCountryId.js");
+const postActivity = require("./postActivity.js");
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+router.use("/countries", getCountryId);
+router.use("/countries", getCountries);
+router.use("/activities", postActivity);
+
+module.exports = router;
